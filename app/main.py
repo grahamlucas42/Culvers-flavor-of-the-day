752cba58906ffbac47ccc725da8a43bb8923b878
Lucas Graham <grahamlucas42@gmail.com>
2026-03-25 10:01:58 -0500

    Adding API_KEY for email endpoint

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 # app/main.py
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, Header, HTTPException
 from app.culvers import get_flavors
 from app.emailer import send_email
+# from dotenv import load_dotenv
 
 app = FastAPI()
+
+# load_dotenv()
+API_KEY = os.getenv("API_KEY")
 
 
 @app.get("/")
@@ -12,7 +17,10 @@ def root():
 
 
 @app.post("/send-emails")
-def trigger_email():
+def trigger_email(x_api_key: str = Header(None)):
+    if x_api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
     flavors = get_flavors()
     send_email(flavors)
     return {"message": f"Sent: {len(flavors)} flavors"}
